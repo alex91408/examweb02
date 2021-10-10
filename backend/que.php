7d4f66c5c9f70d671ef28e8329e162bdd619b3f6
alex91408 <alex9140888@gmail.com>
2021-10-10 17:24:15 +0800

    6

--- backend/que.php
+++ backend/que.php
@@ -1,25 +1,25 @@
-<fieldset style="width:80%;margin:auto">
+<fieldset>
     <legend>新增問卷</legend>
     <form action="api/admin_que.php" method="post">
     <div style="display:flex">
-        <div class="clo" style="width:40%">問卷名稱</div>
-        <div style="width:60%">
-            <input type="text" name="subject"  style="width:95%">
+        <div class="clo">問卷名稱</div>
+        <div>
+            <input type="text" name="subject" >
         </div>
     </div>
-    <div id="options">
-        選項<input type="text" name="opts[]" style="width:80%">
+    <div id="options" class="clo">
+        選項<input type="text" name="opts[]" >
         <input type="button" value="更多" onclick="more()">
     </div>
     <div>
-        <input type="submit" value="新增">
+        <input type="submit" value="新增"> |
         <input type="reset" value="清空">
     </div>
     </form>
 </fieldset>
 <script>
     function more(){
-        let opt=`選項<input type="text" name="opts[]" style="width:80%"><br>`
+        let opt=`選項<input type="text" name="opts[]" ><br>`
         $("#options").prepend(opt)
 
     }
--- frontend/vote.php
+++ frontend/vote.php
@@ -5,7 +5,7 @@ $que=$Que->find($_GET['id']);
 <div>目前位置：首頁 > 問卷調查 > <?=$que['text'];?></div>
 
 <h3><?=$que['text'];?></h3>
-<form action="api/vote.php" method='post'>
+<form action="api/vote.php" method="post">
 <?php
 $opts=$Que->all(['parent'=>$que['id']]);
 foreach($opts as $opt){
